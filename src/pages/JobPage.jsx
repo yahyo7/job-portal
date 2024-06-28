5fe3824b1fcc66bf57fcf43f2b27f8353def6e70
Seven <ahmadaliev.sd@gmail.com>
2024-06-28 20:38:52 +0900

    created single page for a single job

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,6 +1,7 @@
 // import { useState, useEffect } from "react"
 import { useParams, useLoaderData } from "react-router-dom";
-import { FaArrowLeft } from "react-icons/fa";
+import { FaArrowLeft, FaMapMarker } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const JobPage = () => {
   const { id } = useParams();
@@ -10,12 +11,12 @@ const JobPage = () => {
     <>
       <section>
         <div className="container m-auto py-6 px-6">
-          <a
-            href="/jobs.html"
+          <Link
+            to="/jobs"
             className="text-indigo-500 hover:text-indigo-600 flex items-center"
           >
             <FaArrowLeft className="mr-2" /> Back to Job Listings
-          </a>
+          </Link>
         </div>
       </section>
 
@@ -24,13 +25,11 @@ const JobPage = () => {
           <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
             <main>
               <div className="bg-white p-6 rounded-lg shadow-md text-center md:text-left">
-                <div className="text-gray-500 mb-4">Full-Time</div>
-                <h1 className="text-3xl font-bold mb-4">
-                  Senior React Developer
-                </h1>
+                <div className="text-gray-500 mb-4"> {job.type} </div>
+                <h1 className="text-3xl font-bold mb-4">{job.title}</h1>
                 <div className="text-gray-500 mb-4 flex align-middle justify-center md:justify-start">
-                  <i className="fa-solid fa-location-dot text-lg text-orange-700 mr-2"></i>
-                  <p className="text-orange-700">Boston, MA</p>
+                  <FaMapMarker className="text-orange-700 mr-2" />
+                  <p className="text-orange-700">{job.location}</p>
                 </div>
               </div>
 
@@ -39,18 +38,13 @@ const JobPage = () => {
                   Job Description
                 </h3>
 
-                <p className="mb-4">
-                  We are seeking a talented Front-End Developer to join our team
-                  in Boston, MA. The ideal candidate will have strong skills in
-                  HTML, CSS, and JavaScript, with experience working with modern
-                  JavaScript frameworks such as React or Angular.
-                </p>
+                <p className="mb-4">{job.description}</p>
 
                 <h3 className="text-indigo-800 text-lg font-bold mb-2">
                   Salary
                 </h3>
 
-                <p className="mb-4">$70k - $80K / Year</p>
+                <p className="mb-4">{job.salary}</p>
               </div>
             </main>
 
@@ -60,38 +54,34 @@ const JobPage = () => {
               <div className="bg-white p-6 rounded-lg shadow-md">
                 <h3 className="text-xl font-bold mb-6">Company Info</h3>
 
-                <h2 className="text-2xl">NewTek Solutions</h2>
+                <h2 className="text-2xl">{job.company.name}</h2>
 
-                <p className="my-2">
-                  NewTek Solutions is a leading technology company specializing
-                  in web development and digital solutions. We pride ourselves
-                  on delivering high-quality products and services to our
-                  clients while fostering a collaborative and innovative work
-                  environment.
-                </p>
+                <p className="my-2">{job.company.description}</p>
 
                 <hr className="my-4" />
 
                 <h3 className="text-xl">Contact Email:</h3>
 
                 <p className="my-2 bg-indigo-100 p-2 font-bold">
-                  contact@newteksolutions.com
+                  {job.company.contactEmail}
                 </p>
 
                 <h3 className="text-xl">Contact Phone:</h3>
 
-                <p className="my-2 bg-indigo-100 p-2 font-bold">555-555-5555</p>
+                <p className="my-2 bg-indigo-100 p-2 font-bold">
+                  {job.company.contactPhone}
+                </p>
               </div>
 
               {/* <!-- Manage --> */}
               <div className="bg-white p-6 rounded-lg shadow-md mt-6">
                 <h3 className="text-xl font-bold mb-6">Manage Job</h3>
-                <a
-                  href="/add-job.html"
+                <Link
+                  to={`/jobs/edit/${job.id}`}
                   className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
                 >
                   Edit Job
-                </a>
+                </Link>
                 <button className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
                   Delete Job
                 </button>
